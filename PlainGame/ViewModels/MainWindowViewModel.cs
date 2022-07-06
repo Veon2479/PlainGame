e93b23fd086c3cddd5ce633901915578013c523b
Veon2479 <andriei.shuliak@gmail.com>
2022-07-06 14:48:13 +0300

    new project created

---
+++ PlainGame/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PlainGame.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ PlainGame/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PlainGame.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
